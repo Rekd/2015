498690ac3d2236545418c8f9b91c853c0f4e18af
JeffRubin <jeffrub7@gmail.com>
2015-09-18 21:17:41 -0700

    ManualRobotFallClassicCore
    Added code that will be the basis for the fall classic robot.  This code
    is the core of the fall classic robot code, i.e. lift, drive, intake
    functions.  This code does not yet contain autonomous functionality.

---
+++ mark-tests/ManualRobotFallClassicCore/src/LiftSystem.cpp
@@ -0,0 +1,162 @@
+/*
+ * LiftSystem.cpp
+ *
+ *  Created on: Feb 15: 2015
+ *      Author: mll
+ */
+
+#include "LiftSystem.h"
+
+LiftSystem::LiftSystem(CANSpeedController *pLiftMotorBack, CANSpeedController *pLiftMotorFront, CANSpeedController *pLeftIntakeMotor, CANSpeedController *pRightIntakeMotor,
+		DigitalInput *pLiftLimitLow, DigitalInput *pLiftLimitHigh,
+		Encoder *pLiftEncoder, PIDController *pControlLiftBack, PIDController *pControlLiftFront,
+		Joystick *pLiftSysJoystick)
+{
+	//assign objects
+	liftMotorBack = pLiftMotorBack;
+	liftMotorFront = pLiftMotorFront;
+	leftIntakeMotor = pLeftIntakeMotor;
+	rightIntakeMotor = pRightIntakeMotor;
+	liftLimitLow = pLiftLimitLow;
+	liftLimitHigh = pLiftLimitHigh;
+	liftEncoder = pLiftEncoder;
+	controlLiftBack = pControlLiftBack;
+	controlLiftFront = pControlLiftFront;
+	liftSysJoystick = pLiftSysJoystick;
+
+	//initialize local variables
+	liftDir = ZERO_FL; //lift stopped on initialization
+	liftPidOn = false; //lift pid is off at initialization
+	IntakesOff(); //intakes off at initialization
+	atTop = atTopHold = false;
+}
+
+LiftSystem::~LiftSystem()
+{
+}
+
+//Limit Switches
+bool LiftSystem::GetLiftLimitSwitchLow()
+{
+	//invert so that TRUE when limit switch is closed and FALSE when limit switch is open
+	return !(liftLimitLow->Get());
+}
+
+bool LiftSystem::GetLiftLimitSwitchHigh()
+{
+	//invert so that TRUE when limit switch is closed and FALSE when limit switch is open
+	return !(liftLimitHigh->Get());
+}
+
+//Motors
+void LiftSystem::SetLiftMotor(float val)
+{
+	liftMotorBack->Set(LIFT_MOTOR_REV_STATE*val);
+	liftMotorFront->Set(LIFT_MOTOR_REV_STATE*val);
+}
+
+bool LiftSystem::CheckInakeMotorsCurrentSpike()
+{
+	if ((leftIntakeMotor->GetOutputCurrent() > INTAKE_CURRENT_LIMIT) || (rightIntakeMotor->GetOutputCurrent() > INTAKE_CURRENT_LIMIT))
+		return(true);
+	else
+		return(false);
+}
+
+bool LiftSystem::IsButtonPressed(int button)
+{
+	return(liftSysJoystick->GetRawButton(button));
+}
+
+void LiftSystem::IntakesIn()
+{
+	leftIntakeMotor->Set(LEFT_INTAKE_MOTOR_REV_STATE*INTAKE_IN_MOTOR_SPEED);
+	rightIntakeMotor->Set(RIGHT_INTAKE_MOTOR_REV_STATE*INTAKE_IN_MOTOR_SPEED);
+}
+
+void LiftSystem::IntakesOut()
+{
+	leftIntakeMotor->Set(LEFT_INTAKE_MOTOR_REV_STATE*INTAKE_OUT_MOTOR_SPEED);
+	rightIntakeMotor->Set(RIGHT_INTAKE_MOTOR_REV_STATE*INTAKE_OUT_MOTOR_SPEED);
+}
+
+void LiftSystem::IntakesOff()
+{
+	leftIntakeMotor->Set(MOTOR_STOP);
+	rightIntakeMotor->Set(MOTOR_STOP);
+}
+
+//Main
+void LiftSystem::Update()
+{
+	//intakes
+	if(IsButtonPressed(INTAKES_IN_BUTTON))
+		IntakesIn();
+	if(IsButtonPressed(INTAKES_OUT_BUTTON))
+		IntakesOut();
+	if(IsButtonPressed(INTAKES_OFF_BUTTON) || CheckInakeMotorsCurrentSpike())
+		IntakesOff();
+
+	//lift
+	//
+	//prevent bouncing at the top
+	if(GetLiftLimitSwitchHigh())
+		atTop = true;
+	//move the lift away from the top
+	if(atTop)
+	{
+		curLiftPos = liftEncoder->GetDistance();
+		targetLiftPos = curLiftPos - HALF_INCH_OFFSET;
+		controlLiftBack->Enable();
+		controlLiftBack->SetSetpoint(targetLiftPos);
+		controlLiftFront->Enable();
+		controlLiftFront->SetSetpoint(targetLiftPos);
+		atTop = false;
+		atTopHold = true;
+	}
+
+	liftDir = liftSysJoystick->GetY();
+
+	if(atTopHold) //require a lift down command from the lift joystick before allowing the lift to move out of hold
+	{
+		if(liftDir < 0.0) //exit lift hold condition
+		{
+			controlLiftBack->Disable();
+			controlLiftFront->Disable();
+			atTopHold = false;
+		}
+	}
+	else
+	{
+		//Filter deadband
+		if ((liftDir > LIFT_DB_LOW) && (liftDir < LIFT_DB_HIGH)) //in the deadband so hold current position
+		{
+			if(!liftPidOn)
+			{
+				curLiftPos = liftEncoder->GetDistance();
+				controlLiftBack->Enable();
+				controlLiftBack->SetSetpoint(curLiftPos);
+				controlLiftFront->Enable();
+				controlLiftFront->SetSetpoint(curLiftPos);
+				liftPidOn = true;
+			}
+		}
+		else //not in the deadband so do not hold current position
+		{
+			if(liftPidOn)
+			{
+				controlLiftBack->Disable();
+				controlLiftFront->Disable();
+				liftPidOn = false;
+			}
+
+			//manual control
+			if ((liftDir > ZERO_FL) && !GetLiftLimitSwitchHigh() && !atTop)  // move up
+				SetLiftMotor(LIFT_MOTOR_REV_STATE*LIFT_MOTOR_SPEED_UP);
+			else if((liftDir < ZERO_FL) && !GetLiftLimitSwitchLow())  // move down
+				SetLiftMotor(LIFT_MOTOR_REV_STATE*LIFT_MOTOR_SPEED_DOWN); //fixed lift down speed
+			else
+				SetLiftMotor(MOTOR_STOP); //stop
+		}
+	}
+}
---
+++ mark-tests/ManualRobotFallClassicCore/src/LiftSystem.h
@@ -0,0 +1,51 @@
+/*
+ * LiftSystem.h
+ *
+ *  Created on: Feb 15, 2015
+ *      Author: mll
+ */
+
+#ifndef LIFTSYSTEM_H_
+#define LIFTSYSTEM_H_
+
+#include "WPILib.h"
+#include "Constants.h"
+
+class LiftSystem {
+public:
+	LiftSystem(CANSpeedController *pLiftMotorBack, CANSpeedController *pLiftMotorFront, CANSpeedController *pLeftIntakeMotor, CANSpeedController *pRightIntakeMotor,
+			DigitalInput *pLiftLimitLow, DigitalInput *pLiftLimitHigh,
+			Encoder *pLiftEncoder, PIDController *pControlLiftBack, PIDController *pControlLiftFront,
+			Joystick *pLiftSysJoystick);
+	virtual ~LiftSystem();
+	void Update();
+
+private:
+// local motors, switches and joysticks
+	CANSpeedController	*liftMotorBack, *liftMotorFront, *leftIntakeMotor, *rightIntakeMotor;
+	DigitalInput *liftLimitLow, *liftLimitHigh;
+	Encoder *liftEncoder;
+	PIDController *controlLiftBack, *controlLiftFront;
+	Joystick    *liftSysJoystick;
+
+// other private variables
+	float liftDir; //value from the joystick to determine the lift direction (i.e. forward or backwards or stopped)
+	float curLiftPos; //current lift encoder position
+	float targetLiftPos;
+	bool liftPidOn;
+
+	bool atTop, atTopHold; //atTop indicates the lift has hit the upper limit switch; atTopHold indicates that automatic action was taken to move the lift away from the top and hold it in a safe state (i.e. to prevent repeated hits)
+
+	//prototypes
+	float DistToSetpoint();
+	bool GetLiftLimitSwitchLow();
+	bool GetLiftLimitSwitchHigh();
+	void SetLiftMotor(float val);
+	bool CheckInakeMotorsCurrentSpike();
+	bool IsButtonPressed(int button);
+	void IntakesIn();
+	void IntakesOut();
+	void IntakesOff();
+};
+
+#endif /* LIFTSYSTEM_H_ */
